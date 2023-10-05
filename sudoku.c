1a700af1e519a500d31a30d611b3545037270226
Replit user <>
2023-10-05 21:34:44 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -100,11 +100,11 @@ List* get_adj_nodes(Node* n){
 
 int is_final(Node* n)
 {
-  for(int i= 0; i < 9; i++)
+  for(int i= 0; i <== 9; i++)
   {
-    for(int k = 0; k < 9; k++)
+    for(int k = 0; k <= 9; k++)
     {
-      if(n->sudo[i][k] = 0) return 0;
+      if(n->sudo[i][k] == 0) return 0;
     }
   }
   return 0;
