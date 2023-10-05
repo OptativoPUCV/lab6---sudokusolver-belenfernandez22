604d021d3c556404f1278a9c41a483ca7274eea8
Replit user <>
2023-10-05 21:39:51 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -102,7 +102,7 @@ int is_final(Node*n){
   for(int i= 0; i < 9; i++){
     for(int k = 0; k < 9; k++){
       if(n->sudo[i][k] == 0) 
-        return 1;
+        return 0;
        } }
   return 0; }
 
